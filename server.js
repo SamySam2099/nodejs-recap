129916ed16bc3a285bf98cc897bfee456124abde
SamySam2099 <38728942+SamySam2099@users.noreply.github.com>
2025-06-08 22:06:08 +0100

    Add routing

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import http from 'http'
 const PORT = process.env.PORT
 
+const ROUTE_TO_HTML = {
+  HOMEPAGE: '<h1>Homepage</h1>',
+  ABOUT: '<h1>About</h1>',
+  NOT_FOUND: '<h1>Not Found</h1>',
+}
+
 const server = http.createServer((req, res) => {
   // Content - Type header tells which type of data is being sent in the response
   // It tells the client how to interpret the data
@@ -8,13 +14,21 @@ const server = http.createServer((req, res) => {
 
   // Unlike express, node.js would just run the command for any
   // endpoint like /, /about, /anything
-  console.log(req.url)
-  console.log(req.method)
+  //console.log(req.url)
+  //console.log(req.method)
 
   //res.write('Hello world')
 
-  res.writeHead(200, { 'Content-Type': 'application/json' })
-  res.end(JSON.stringify({ message: 'Hello' }))
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(ROUTE_TO_HTML.HOMEPAGE)
+  } else if (req.url === '/about') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(ROUTE_TO_HTML.ABOUT)
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' })
+    res.end(ROUTE_TO_HTML.NOT_FOUND)
+  }
 })
 
 server.listen(PORT, () => {
